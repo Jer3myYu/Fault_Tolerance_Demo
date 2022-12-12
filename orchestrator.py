05a504f4edbb50047f2139de4a0cbae79712a4dd
Jeremy Yu <90216803+Jer3myYu@users.noreply.github.com>
2022-12-11 23:41:10 -0500

    feat: prototype

---
+++ orchestrator.py
@@ -0,0 +1,38 @@
+"""Orchestrator MQTT client."""
+import paho.mqtt.client as paho
+import sys
+
+broker = "192.168.1.154"
+
+def on_message(client, userdata, msg):
+    """Callback for receive msg."""
+    print(msg.topic + ": " + msg.payload.decode())
+
+def on_log(client, userdata, level, buf):
+    """Callback for logging."""
+    print("log: " + buf)
+
+def on_connect(client, userdata, flags, rc):
+    """Callback for connect to broker."""
+    if rc == 0:
+        print("connect OK")
+    else:
+        print("Could not connect to MQTT Broker!")
+
+def on_disconnect(client, userdata, flags, rc=0):
+    """Callback for disconnect from broker."""
+    print("Disconnected return code " + str(rc))
+
+if __name__ == '__main__':
+    client = paho.Client()
+    client.on_message = on_message
+    client.on_connect = on_connect
+    client.on_disconnect = on_disconnect
+    client.on_log = on_log
+
+    client.connect(broker, port = 1883, keepalive=60)
+    client.subscribe("runtime/keepalive")
+
+
+
+    
