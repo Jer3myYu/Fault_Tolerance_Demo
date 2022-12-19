164dfa4d3c0b34cb5b2ae0ceb9f805c6d1104928
Zhanfan Yu <jeremy@Zhanfans-MBP.attlocal.net>
2022-12-19 00:34:41 -0800

    feat: make keepalive an another thread use UDP socket

--- orchestrator.py
+++ orchestrator.py
@@ -4,13 +4,16 @@ import sys
 
 broker = "192.168.1.154"
 
+
 def on_message(client, userdata, msg):
     """Callback for receive msg."""
     print(msg.topic + ": " + msg.payload.decode())
 
+
 def on_log(client, userdata, level, buf):
     """Callback for logging."""
     print("log: " + buf)
+
 
 def on_connect(client, userdata, flags, rc):
     """Callback for connect to broker."""
@@ -19,9 +22,11 @@ def on_connect(client, userdata, flags, rc):
     else:
         print("Could not connect to MQTT Broker!")
 
+
 def on_disconnect(client, userdata, flags, rc=0):
     """Callback for disconnect from broker."""
     print("Disconnected return code " + str(rc))
+
 
 if __name__ == '__main__':
     client = paho.Client()
@@ -30,7 +35,7 @@ if __name__ == '__main__':
     client.on_disconnect = on_disconnect
     client.on_log = on_log
 
-    client.connect(broker, port = 1883, keepalive=60)
+    client.connect(broker, port=1883, keepalive=60)
     client.subscribe("runtime/keepalive")
 
 
